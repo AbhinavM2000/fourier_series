81e38bc9b2238a49c255ddbfe2185806cb527d27
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-01-03 13:41:02 +0530

    Update n-terms_stp_fn_corrected.c

--- n-terms_stp_fn_corrected.c
+++ n-terms_stp_fn_corrected.c
@@ -2,10 +2,10 @@
 #include<math.h>
 #define PI 3.14159265
 
-double calc_100_terms(double x, double an[100], double bn[100], int n) { //function that evaluates ao/2 + Σ 1 to 100 (an cosnx + bn sin nx) at x
+double calc_n_terms(double x, double an[100], double bn[100], int n) { //function that evaluates ao/2 + Σ 1 to n (an cosnx + bn sin nx) at x
   double fxxx, ao;
   fxxx = 0;
-  return ((an[n]) * cos(n * x)) + ((bn[n]) * sin(n * x)); // returned for printing to txt
+  return ((an[n]) * cos(n * x)) + ((bn[n]) * sin(n * x)); // returned to line 37 for summation
 }
 
 void getData(double numPt, double step, double p, double q, double an[100], double bn[100], int num_coeff) { //received an and bn
@@ -23,7 +23,7 @@ void getData(double numPt, double step, double p, double q, double an[100], doub
     return;
   }
 
-  for (double i = p; i <= q; i = i + step) { //for loop #1
+  for (double i = p; i <= q; i = i + step) { //for loop #1 to vary x in f(x)
 
     //Plotting step fn
     if (i >= -PI && i < 0)
@@ -32,12 +32,12 @@ void getData(double numPt, double step, double p, double q, double an[100], doub
       fx_real = 1;
     //Saving (x,fx_real) to txt file
     fprintf(fp2, "%lf %lf\n", i, fx_real);
-    for (double k = 1; k <= num_coeff; k++) { //for loop #2
+    for (double k = 1; k <= num_coeff; k++) { //for loop #2 to sum up coefficients from 1 to num_coeff for that value of x
       //Fourier series function
-      fx_fourier = fx_fourier + calc_100_terms(i, an, bn, k); //passed an bn and x , here k is for representing the kth coefficient of an bn 
+      fx_fourier = fx_fourier + calc_n_terms(i, an, bn, k); //passed an bn and x , here k is for representing the kth coefficient of an bn 
     }
     //Saving (x,fourier) to txt file
-    fprintf(fp, "%lf %lf\n", i, 1 * ((ao / 2) + fx_fourier)); // ao/2 added
+    fprintf(fp, "%lf %lf\n", i, 1 * ((ao / 2) + fx_fourier)); // ao/2 added to the final f(x) value
     fx_fourier = 0; // reset for the next value of x
   }
   fclose(fp);
@@ -56,8 +56,8 @@ int main() {
 
   //Calculating required number of coefficients for an and bn for this function
   while (N <= num_coeff) {
-    an[N] = -(2 / N / PI) * sin(N * PI * 0.5);
-    bn[N] = (2 / N / PI) * (cos(N * PI * 0.5) - (pow(-1, N)));
+    an[N] = -(2 / N / PI) * sin(N * PI * 0.5);  //Formula for an
+    bn[N] = (2 / N / PI) * (cos(N * PI * 0.5) - (pow(-1, N)));   //Formula for bn
     N++;
   }
   //limits
