e7a94ab2ca8bc1e6ea33324751f9b141d2c034f5
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-01-04 11:04:47 +0530

    Update and rename f_series_explainer.c to f_series_stp2.c

--- f_series_stp2.c
+++ f_series_stp2.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
+
 #include<math.h>
+
 #define PI 3.14159265
 
 double calc_100_terms(double x, double an[100], double bn[100], int n) { //function that evaluates ao/2 + Σ 1 to 100 (an cosnx + bn sin nx) at x
   return ((an[n]) * cos(n * x)) + ((bn[n]) * sin(n * x)); // returned to line 37 for summation
 }
 
-void getData(int numPt, double p, double q, double an[100], double bn[100],int num_coeff) { //received an and bn
+void getData(int numPt, double p, double q, double an[100], double bn[100], int num_coeff) { //received an and bn
   double fx_fourier, fx_real, ao, step;
-step = (q - p) / numPt; //calculating step
-fx_fourier = 0;
+  step = (q - p) / numPt; //calculating step
+  fx_fourier = 0;
   ao = 1; //ao for this function
   int k;
   k = 1;
@@ -23,7 +25,7 @@ fx_fourier = 0;
     return;
   }
 
-  for (double i =p; i <= q; i = i + step) { //for loop #1 to vary x in f(x)
+  for (double i = p; i <= q; i = i + step) { //for loop #1 to vary x in f(x)
 
     //Plotting step fn
     if (i >= -PI && i < 0)
@@ -37,16 +39,13 @@ fx_fourier = 0;
       fx_fourier = fx_fourier + calc_100_terms(i, an, bn, k); //passed an bn and x , here k is for representing the kth coefficient of an bn 
     }
     //Saving (x,fourier) to txt file
-printf(" %lf %lf \n",i, fx_fourier);
-    fprintf(fp, "%lf %lf\n", i,  ((ao / 2) + fx_fourier)); // ao/2 added to the final f(x) value
+    printf(" %lf %lf \n", i, fx_fourier);
+    fprintf(fp, "%lf %lf\n", i, ((ao / 2) + fx_fourier)); // ao/2 added to the final f(x) value
     fx_fourier = 0; // reset for the next value of x
   }
   fclose(fp);
   fclose(fp2);
 }
-
-
-
 
 int main() {
   int numPt, N, num_coeff;
@@ -60,8 +59,8 @@ int main() {
 
   //Calculating required number of coefficients for an and bn for this function
   while (N <= num_coeff) {
-    an[N] = 0;                     //Formula for an
-    bn[N] = (1-pow(-1,N))/PI/N;   //Formula for bn
+    an[N] = 0; //Formula for an
+    bn[N] = (1 - pow(-1, N)) / PI / N; //Formula for bn
     N++;
   }
 
@@ -69,7 +68,7 @@ int main() {
   p = -PI;
   q = PI;
   //passed an and bn
-  getData(numPt, p, q, an, bn,num_coeff);
+  getData(numPt, p, q, an, bn, num_coeff);
   printf("\nDone ! Run plot.py \n");
   return 0;
 }
