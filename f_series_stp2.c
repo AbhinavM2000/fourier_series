a2a71a123f6ed5feb90ebdf18fa729a64f9e116c
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-01-04 11:23:01 +0530

    Update f_series_stp2.c

--- f_series_stp2.c
+++ f_series_stp2.c
@@ -3,16 +3,15 @@
 #define PI 3.14159265
 
 double calc_n_terms(double x, double an[100], double bn[100], int n) { //function that evaluates ao/2 + Σ 1 to 100 (an cosnx + bn sin nx) at x
-  return ((an[n]) * cos(n * x)) + ((bn[n]) * sin(n * x)); // returned to line 37 for summation
+  return ((an[n]) * cos(n * x)) + ((bn[n]) * sin(n * x)); // returned to line 40 for summation
 }
 
 void getData(int numPt, double p, double q, double an[100], double bn[100], int num_coeff) { //received an and bn
   double fx_fourier, fx_real, ao, step;
-  int k;
+  int n;
   step = (q - p) / numPt; //calculating step
   ao = 1; //ao for this function
-  k = 1;
-  fx_fourier = 0;
+  n = 1;
 
   //Opening files
   FILE * fp;
@@ -20,39 +19,47 @@ void getData(int numPt, double p, double q, double an[100], double bn[100], int 
   FILE * fp2;
   fp2 = fopen("data2.txt", "w");
 
-  for (double i = p; i <= q; i = i + step) { //for loop #1 to vary x in f(x)
+  for (double x = p; x <= q; x = x + step) { //for loop #1 to vary x in f(x)
 
     //Plotting step fn
-    if (i >= -PI && i < 0)
+    if (x >= -PI && x < 0)
       fx_real = 0;
-    if (i <= PI && i > 0)
+    if (x <= PI && x > 0)
       fx_real = 1;
     //Saving (x,fx_real) to txt file
-    fprintf(fp2, "%lf %lf\n", i, fx_real);
+    fprintf(fp2, "%lf %lf\n", x, fx_real);
 
 
-    for (int k = 1; k <= num_coeff; k++) { //for loop #2 to sum up coefficients from 1 to num_coeff for that value of x
+    for (int n = 1; n <= num_coeff; n++) { //for loop #2 to sum up coefficients from 1 to num_coeff for that value of x
       //Fourier series function
-      fx_fourier = fx_fourier + calc_n_terms(i, an, bn, k); //passed an bn and x , here k is for representing the kth coefficient of an bn 
+      fx_fourier = fx_fourier + calc_n_terms(x, an, bn, n); //passed an bn and x
     }
+
     //Saving (x,fourier) to txt file
-    printf(" %lf %lf \n", i, fx_fourier);
-    fprintf(fp, "%lf %lf\n", i, ((ao / 2) + fx_fourier)); // ao/2 added to the final f(x) value
+    fprintf(fp, "%lf %lf \n", x, ((ao / 2) + fx_fourier)); // ao/2 added to the final f(x) value
+    
     fx_fourier = 0; // reset for the next value of x
   }
   fclose(fp);
   fclose(fp2);
 }
 
+
+
 int main() {
   int numPt, N, num_coeff;
+  double p, q, an[100], bn[100];
   N = 1;
+
+  //limits
+  p = -PI;
+  q = PI;
+
   printf("Enter the number of points e.g. 100\n");
   scanf("%d", & numPt);
   printf("Enter the number of coefficients e.g. 100\n");
   scanf("%d", & num_coeff);
 
-  double p, q, an[100], bn[100];
 
   //Calculating required number of coefficients for an and bn for this function
   while (N <= num_coeff) {
@@ -61,11 +68,11 @@ int main() {
     N++;
   }
 
-  //limits
-  p = -PI;
-  q = PI;
+  
   //passed an and bn
   getData(numPt, p, q, an, bn, num_coeff);
+
+
   printf("\nDone ! Run plot.py \n");
   return 0;
 }
