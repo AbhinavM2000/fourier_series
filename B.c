18c77494b96d23413b298b7321dffe9cae5f941b
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-01-06 13:06:58 +0530

    Update B.c

--- B.c
+++ B.c
@@ -20,12 +20,10 @@ void getData(double numPt, double step, double p, double q) {
   for (double i = p ; i <= q; i = i + step) {
 //Fourier series terms here (Check the fourier series, I'm not sure about this one)
    t1= 1/PI;
-   t2= cos(2*i);
-   t3= cos(4*i);
-    t4=cos(6*i);
-t2=t2/3;
-t3=t3/15;
-t4=t4/35;
+   t2= 0.5*sin(i) - 2* (cos(2*i))/3/PI;
+   t3= 0.5*sin(i) - 2* (cos(4*i))/15/PI;
+	t4=t3= 0.5*sin(i) - 2* (cos(6*i))/35/PI;
+
 	
 
 //Plotting 0 and sin x
@@ -36,7 +34,7 @@ if(i<0&&i>-PI)
 //Saving (x,fx_real) to txt file
     fprintf(fp2, "%lf %lf\n", i, fx_real);
 //Fourier series function
-    fx_fourier =  t1 + (2/PI)*(-t2 - t3 - t4) ;
+    fx_fourier =  t1 + t2+t3+t4 ;
 //Saving (x,fourier) to txt file
     fprintf(fp, "%lf %lf\n", i, fx_fourier);
 
