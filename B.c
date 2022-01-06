45b49136a9edd16446ef819f97af84a53f63ad8e
Abhinav M <46239775+AbhinavM2000@users.noreply.github.com>
2022-01-06 13:32:58 +0530

    Update B.c

--- B.c
+++ B.c
@@ -5,7 +5,7 @@
 #define PI 3.14159265
 
 void getData(double numPt, double step, double p, double q) {
-  double t1, t2, t3,t4, fx_fourier, fx_real;
+  double t1, t2, t3,t4,t5, fx_fourier, fx_real;
 
 //Opening files
   FILE * fp;
@@ -19,12 +19,18 @@ void getData(double numPt, double step, double p, double q) {
 
   for (double i = p ; i <= q; i = i + step) {
 //Fourier series terms here (Check the fourier series, I'm not sure about this one)
-   t1= 1/PI;
-   t2= 0.5*sin(i) - 2* (cos(2*i))/3/PI;
-   t3= 0.5*sin(i) - 2* (cos(4*i))/15/PI;
-	t4=t3= 0.5*sin(i) - 2* (cos(6*i))/35/PI;
+ 
+  t1= (1/PI) + 0.5*sin(i);
+   t2=   (0.67)*cos(2*i);
+   t3=   (0.134)*cos(4*i);
+   t4=   (0.0571)*cos(6*i);
+   t5=   (0.0317)*cos(8*i);
 
-	
+t2=t2/PI;
+t3=t3/PI;
+t4=t4/PI;
+t5=t5/PI;	
+
 
 //Plotting 0 and sin x
 if(i>0&&i<PI)
@@ -34,7 +40,7 @@ if(i<0&&i>-PI)
 //Saving (x,fx_real) to txt file
     fprintf(fp2, "%lf %lf\n", i, fx_real);
 //Fourier series function
-    fx_fourier =  t1 + t2+t3+t4 ;
+    fx_fourier =  t1 - t2 - t3 - t4 - t5;
 //Saving (x,fourier) to txt file
     fprintf(fp, "%lf %lf\n", i, fx_fourier);
 
